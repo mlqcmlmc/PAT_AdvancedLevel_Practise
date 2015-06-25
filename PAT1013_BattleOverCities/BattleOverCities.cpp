026f5ac87dd9ff0527638fc7ec175359ac1a94f8
mlqcmlmc <345008702@qq.com>
2015-06-25 23:17:08 +0800

    add 1014,modify 1013

--- PAT1013_BattleOverCities/BattleOverCities.cpp
+++ PAT1013_BattleOverCities/BattleOverCities.cpp
@@ -1,52 +1,132 @@
-/*使用深度优先搜索方法，当然也可以使用广度优先算法来做*/
+///*使用深度优先搜索方法，当然也可以使用广度优先算法来做*/
+//#include<iostream>
+//#include <cstring>
+//using namespace std;
+//
+//
+//#define MAX 1000
+//int matrix[MAX][MAX] = { 0 };
+//int visited[MAX];
+//int N, M, K;
+//
+//void DFS(int v)
+//{
+//    visited[v] = 1;
+//    for (int i = 1; i <= N; ++i)
+//    {
+//        if (visited[i] == 0 && matrix[v][i] == 1)
+//        {
+//            DFS(i);
+//        }
+//    }
+//}
+//int main()
+//{
+//
+//    cin >> N >> M >> K;
+//    int index1, index2;
+//    for (int i = 0; i < M; ++i)
+//    {
+//        cin >> index1 >> index2;
+//        matrix[index1][index2] = 1;
+//        matrix[index2][index1] = 1;
+//    }
+//    int check;
+//    for (int i = 0; i < K; ++i)
+//    {
+//        int count = 0;
+//        cin >> check;
+//        memset(visited, 0, sizeof(visited));
+//        visited[check] = 1;     //使得被占领的城市标记为零，则深度优化算法不会通过该定点了
+//        for (int j = 1; j <= N; ++j)
+//        {
+//            if (visited[j] == 0)
+//            {
+//                DFS(j);
+//                ++count;
+//            }
+//        }
+//        cout << count-1 << endl;
+//    }
+//    return 0;
+//}
+
+
+
+/*使用并查集来实现*/
 #include<iostream>
 #include <cstring>
 using namespace std;
 
-
-#define MAX 1000
-int matrix[MAX][MAX] = { 0 };
-int visited[MAX];
+#define MAXCITY 1000
+#define MAXHIGHWAY 500000 //这里是使用了C1000中取2 ，然后是1000*999/(2*1)，来计算的
+int uset[MAXCITY];
 int N, M, K;
-
-void DFS(int v)
+struct HighWay
 {
-    visited[v] = 1;
-    for (int i = 1; i <= N; ++i)
+    int x;
+    int y;
+}highways[MAXHIGHWAY];
+
+/*MakeSet()：建立一个新的并查集，其中包含 N 个单元素集合*/
+void MakeSet() 
+{
+    for (int i = 0; i <= N; i++)
     {
-        if (visited[i] == 0 && matrix[v][i] == 1)
-        {
-            DFS(i);
-        }
+        uset[i] = i;
     }
 }
+
+/*FindSet(a)：找到元素 x 所在的集合的代表，该操作也可以用于判断两个元素是否位于同一个集合，只要将它们各自的代表比较一下就可以了。*/
+int FindSet(int a)
+{
+    if (uset[a] != a)
+    {
+        uset[a] = FindSet(uset[a]);
+    }
+    return uset[a];
+}
+
+
+void JoinSet(int x, int y)
+{
+    x = FindSet(x);
+    y = FindSet(y);
+    if (x != y) 
+    {
+        uset[y] = x;
+    }
+}
+
 int main()
 {
-
     cin >> N >> M >> K;
-    int index1, index2;
     for (int i = 0; i < M; ++i)
     {
-        cin >> index1 >> index2;
-        matrix[index1][index2] = 1;
-        matrix[index2][index1] = 1;
+        cin >> highways[i].x >> highways[i].y;
     }
+
     int check;
     for (int i = 0; i < K; ++i)
     {
-        int count = 0;
         cin >> check;
-        memset(visited, 0, sizeof(visited));
-        visited[check] = 1;     //使得被占领的城市标记为零，则深度优化算法不会通过该定点了
-        for (int j = 1; j <= N; ++j)
+        MakeSet();
+        for (int j = 0; j < M; ++j)
         {
-            if (visited[j] == 0)
+            if (highways[j].x != check && highways[j].y != check)////对于含有要检查的点的路径的两端点不加入并查集
             {
-                DFS(j);
-                ++count;
+                JoinSet(highways[j].x, highways[j].y);
             }
         }
-        cout << count-1 << endl;
+        int count = 0;
+        for (int j = 1; j <= N; ++j)
+        {
+            if (uset[j] == j && uset[j] != check)
+            {
+                count++; //统计除check节点外的连通分量个数
+            }
+        }
+        printf("%d\n", count - 1);
     }
     return 0;
 }
---
+++ PAT1014_WaitinginLine/WaitinginLine.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include"stdio.h"
+using namespace std;
+
+int N, M, K, Q;
+
+
+struct Window
+{
+    int mark = 0;
+    int window_first = 0;
+    int window_last = 0;
+    queue<int> window_time;
+};
+
+bool cmp(Window a, Window b)
+{
+    if (a.window_first == b.window_first)
+    {
+        return a.mark < b.mark;
+    }
+    else
+    {
+        return a.window_first < b.window_first;
+    }
+}
+int main()
+{
+
+    cin >> N >> M >> K >> Q;
+    vector<int> left_time, serve_time;
+    vector<Window> windows;
+    windows.resize(N);
+    for (int i = 0; i < N; ++i)
+    {
+        windows[i].mark = i;
+    }
+    //vector<int> window_first, window_last;
+    //window_first.resize(N);
+    //window_last.resize(N);
+    int temp_time;
+    for (int i = 0; i < K; ++i)
+    {
+        cin >> temp_time;
+        if (windows[i%N].window_time.size() < M - 1)
+        {
+            windows[i%N].window_time.push(temp_time);
+            windows[i%N].window_last += temp_time;			//计算每个窗口的最后一个人的离开时间
+
+            left_time.push_back(windows[i%N].window_last);	//保存每个顾客的离开时间
+        }
+        else if (windows[i%N].window_time.size() == M - 1)
+        {
+            windows[i%N].window_time.push(temp_time);
+            windows[i%N].window_last += temp_time;			//计算每个窗口的最后一个人的离开时间
+
+            auto data = windows[i%N].window_time.front();
+            windows[i%N].window_first += data;				//计算每个窗口的第一个人的离开时间
+
+            left_time.push_back(windows[i%N].window_last);	//保存每个顾客的离开时间
+        }
+        else
+        {
+            sort(windows.begin(), windows.end(), cmp);		//排序，每个窗口第一个人将要离开的最小时间
+
+            windows[0].window_time.push(temp_time);
+            windows[0].window_last += temp_time;			//计算每个窗口的最后一个人的离开时间
+
+            windows[0].window_time.pop();
+            auto data = windows[0].window_time.front();
+            windows[0].window_first += data;				//计算每个窗口的第一个人的离开时间
+
+
+            left_time.push_back(windows[0].window_last);	//保存每个顾客的离开时间
+        }
+        serve_time.push_back(left_time[i] - temp_time);
+    }
+    int out_customer;
+    int shi;
+    int fen;
+    for (int i = 0; i < Q; ++i)
+    {
+        cin >> out_customer;
+        // 本题容易出错的地方：是在17:00及以后开始服务的客户输出"Sorry"，而不是17 : 00之前结束服务的输出"Sorry"；
+        //如客户cId是16:59开始服务，服务时间为2，则输出的应该是17:01，而不是"Sorry"。
+        if (serve_time[out_customer - 1] < 540)
+        {
+            shi = 8 + left_time[out_customer - 1] / 60;
+            fen = left_time[out_customer - 1] % 60;
+            printf("%02d:%02d", shi, fen);
+            printf("\n");
+        }
+        else
+        {
+            cout << "Sorry" << endl;
+        }
+    }
+
+    return 0;
+}
