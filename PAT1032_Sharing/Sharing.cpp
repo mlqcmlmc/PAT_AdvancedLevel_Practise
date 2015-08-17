20db745d9435535a49d68e90a88d11226f4806d4
mlqcmlmc <345008702@qq.com>
2015-08-17 22:46:20 +0800

    add 1032

---
+++ PAT1032_Sharing/Sharing.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+#define num 100000
+int flag[num] = { 0 };
+int main()
+{
+    long int addr1, addr2, temp1, temp2;
+    char c;
+    long int N;
+    cin >> addr1 >> addr2 >> N;
+    map<long int, long int> map_addrs;
+    for (long int i = 0; i < N; ++i)
+    {
+        cin >> temp1 >> c >> temp2;
+        map_addrs.insert(make_pair(temp1, temp2));
+    }
+    long int key = addr1;
+    while (key != -1)
+    {
+        flag[key] = 1;
+        key = map_addrs[key];
+    }
+    key = addr2;
+    while (key != -1)
+    {
+        if (flag[key] == 1)
+        {
+            printf("%05d\n", key);
+            break;
+        }
+        key = map_addrs[key];
+    }
+    if (key == -1)
+    {
+        printf("-1\n");
+    }
+    return 0;
+}
