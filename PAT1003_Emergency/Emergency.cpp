139bb12facde1fb34dfc299c5461abab1327b9eb
mlqcmlmc <345008702@qq.com>
2015-09-05 21:51:04 +0800

    add 1003

--- PAT1003_Emergency/Emergency.cpp
+++ PAT1003_Emergency/Emergency.cpp
@@ -1,62 +1,84 @@
-#include<iostream>
-#include<vector>
-#include"stdio.h"
-#include"stdlib.h"
+#include <iostream>  
+
 using namespace std;
-struct EdgeNode
-{
-    int adj_vertex;
-    int road_length;
-    EdgeNode *next_edge;
-};//边表结点
-struct VertexNode
-{
-    int rescue_num;
-    EdgeNode *first_edge;
-};//顶点节点
-struct AlgraphList
-{
-    int nodes;
-    int edges;
-    vector<VertexNode> vertex_list;
-};
 
-void CreatAlgraph(AlgraphList *list)
-{
-    int road_current, road_save;
-    int rescue_num_temp;
-    cin >> list->nodes >> list->edges >> road_current >> road_save;
-    list->vertex_list.resize(list->nodes);
-    for (int i = 0; i < list->nodes; i++)
-    {
-        cin >> rescue_num_temp;
-        list->vertex_list[i].rescue_num = rescue_num_temp;
-        list->vertex_list[i].first_edge = NULL;
-    }
-    //创建无向图的邻接表
-    int c1, c2, L;
-    EdgeNode *c1_new_node, *c2_new_node;
-    for (int i = 0; i < list->edges; i++)
-    {
-        cin >> c1 >> c2 >> L;
-        c1_new_node = (EdgeNode*)malloc(sizeof(EdgeNode));
-        c1_new_node->adj_vertex = c2;
-        c1_new_node->road_length = L;
-        c1_new_node->next_edge = list->vertex_list[c1].first_edge;
-        list->vertex_list[c1].first_edge = c1_new_node;
+const int CITYMAX = 501;
+const int INF = 0x7fffffff;
 
-        c2_new_node = (EdgeNode*)malloc(sizeof(EdgeNode));
-        c2_new_node->adj_vertex = c1;
-        c2_new_node->road_length = L;
-        c2_new_node->next_edge = list->vertex_list[c2].first_edge;
-        list->vertex_list[c2].first_edge = c2_new_node;
+int city[CITYMAX] = { 0 };              //记录各个城市的团队数 
+int road[CITYMAX][CITYMAX] = { 0 };     //记录道路的长度，是一个对角矩阵
+bool isvisited[CITYMAX] = { false };    //记录当前定点是否访问过
+int minLen[CITYMAX] = { 0 };            //从源城市到达index城市的最短路径值  
+int sum[CITYMAX] = { 0 };               //从源城市到达index城市，所能召集的最大团队数  
+int same[CITYMAX] = { 0 };              //从源城市到达index城市,具有相同最短的路径个数  
+void Dij(int source, int dest, int n)
+{
+    int i, t, mm, next;
+    int count = 0;
+    int cur = source;
+    sum[cur] = city[cur];
+    same[cur] = 1;
+    while (count < n - 1)
+    {
+        isvisited[cur] = true;
+        mm = INF;
+        for (i = 0; i < n; i++)
+        {
+            if (isvisited[i])
+            {
+                continue;
+            }
+            if (road[cur][i])
+            {
+                t = minLen[cur] + road[cur][i];
+                if (t < minLen[i] || minLen[i] == 0)    //到达城市i，出现新的最短路径  
+                {
+                    minLen[i] = t;
+                    same[i] = same[cur];                //重新计数,可能到达本节点cur的最短路径有多条  
+                    sum[i] = sum[cur] + city[i];
+                }
+                else if (t == minLen[i])                //到达城市i，出现相同的最短路径  
+                {
+                    same[i] += same[cur];
+                    if (sum[cur] + city[i] > sum[i])    //记下团队数较大的值  
+                        sum[i] = sum[cur] + city[i];
+                }
+            }
+            if (minLen[i] < mm && minLen[i] != 0)
+            {
+                mm = minLen[i];
+                next = i;
+            }
+        }
+        minLen[cur] = mm;
+        //if (next == dest)
+        //  break;
+        cur = next;
+        count++;
     }
+    return;
 }
-
 int main()
 {
-    AlgraphList *list = nullptr;
-    CreatAlgraph(list);
-
+    int N, M, C1, C2;
+    cin >> N >> M >> C1 >> C2;
+    for (int i = 0; i < N;++i)
+    {
+        cin >> city[i];
+    }
+    for (int i = 0; i < M; ++i)
+    {
+        int c1, c2;
+        cin >> c1 >> c2;
+        cin >> road[c1][c2];
+        road[c2][c1] = road[c1][c2];
+    }
+    if (C1 == C2)//若所在地就是目的地 则直接输出结果
+    {
+        cout << 1 << ' ' << city[C1] << endl;
+        return 0;
+    }
+    Dij(C1, C2, N);
+    cout << same[C2] << ' ' << sum[C2] << endl;
     return 0;
 }
